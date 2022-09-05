4a9901dce1895a7d3b1a8b106fc7fbbc8c4f169b
sutsanyuan <tsanyuansu@gmail.com>
2022-09-05 21:37:29 +0800

    vuex install sudo npm i -S vuex@4
    chart inserted
    chartElement.vue
    simple RWD layout

--- src/components/ChartElement.vue
+++ src/components/ChartElement.vue
@@ -58,55 +58,172 @@ import {loadScript} from "vue-plugin-load-script";
 
 //the following code failed
 export default {
-  data: () => ({}),
-  mounted() {
-    // let recaptchaScript = document.createElement("script");
-    // recaptchaScript.setAttribute("src", "./astrochart.js");
-    // document.body.appendChild(recaptchaScript);
+  data: () => ({
+    cs: 500,
+    astroData: {
+      planets: {
+        Lilith: [18],
+        Chiron: [18],
+        Pluto: [63],
+        Neptune: [110, 0.2],
+        Uranus: [318],
+        Saturn: [201, -0.2],
+        Jupiter: [192],
+        Mars: [210],
+        Moon: [268],
+        Sun: [281],
+        Mercury: [312],
+        Venus: [330],
+        NNode: [2],
+      },
+      cusps: [296, 350, 30, 56, 75, 94, 116, 170, 210, 236, 255, 274],
+    },
+  }),
+  props: {
+    chartSize: Number, //read only
+    testString: String,
+  },
+  created() {
     loadScript("public/js/astrochart.js")
       .then(() => {
         // Script is loaded, do something
-        console.log("loaded");
-        var data = {
-          planets: {
-            Lilith: [18],
-            Chiron: [18],
-            Pluto: [63],
-            Neptune: [110, 0.2],
-            Uranus: [318],
-            Saturn: [201, -0.2],
-            Jupiter: [192],
-            Mars: [210],
-            Moon: [268],
-            Sun: [281],
-            Mercury: [312],
-            Venus: [330],
-            NNode: [2],
-          },
-          cusps: [296, 350, 30, 56, 75, 94, 116, 170, 210, 236, 255, 274],
-        };
-        // window.onload = function () {
-        var radix = new astrology.Chart("paper", 600, 600).radix(data);
-        // Aspect calculation
-        // default is planet to planet, but it is possible add some important points:
-        radix.addPointsOfInterest({
-          As: [data.cusps[0]],
-          Ic: [data.cusps[3]],
-          Ds: [data.cusps[6]],
-          Mc: [data.cusps[9]],
-        });
-        radix.aspects();
+        console.log("created loaded");
+
         //};
       })
       .catch(() => {
         // Failed to fetch script
+        console.log("chart failed to load");
       });
   },
-  methods: {},
+  mounted() {
+    // let recaptchaScript = document.createElement("script");
+    // recaptchaScript.setAttribute("src", "./astrochart.js");
+    // document.body.appendChild(recaptchaScript);
+    if (window.innerWidth < 768) {
+      this.cs = window.innerWidth * 0.9;
+    }
+    loadScript("public/js/astrochart.js")
+      .then(() => {
+        // Script is loaded, do something
+        console.log("loaded");
+        console.log(this.testString);
+        // var data = {
+        //   planets: {
+        //     Lilith: [18],
+        //     Chiron: [18],
+        //     Pluto: [63],
+        //     Neptune: [110, 0.2],
+        //     Uranus: [318],
+        //     Saturn: [201, -0.2],
+        //     Jupiter: [192],
+        //     Mars: [210],
+        //     Moon: [268],
+        //     Sun: [281],
+        //     Mercury: [312],
+        //     Venus: [330],
+        //     NNode: [2],
+        //   },
+        //   cusps: [296, 350, 30, 56, 75, 94, 116, 170, 210, 236, 255, 274],
+        // };
+        // window.onload = function () {
+        var radix = new astrology.Chart("paper", this.cs, this.cs).radix(
+          this.astroData
+        );
+        // Aspect calculation
+        // default is planet to planet, but it is possible add some important points:
+        radix.addPointsOfInterest({
+          As: [this.astroData.cusps[0]],
+          Ic: [this.astroData.cusps[3]],
+          Ds: [this.astroData.cusps[6]],
+          Mc: [this.astroData.cusps[9]],
+        });
+        radix.aspects();
+
+        //};
+      })
+      .catch(() => {
+        // Failed to fetch script
+        console.log("chart failed to load");
+      });
+  },
+  updated() {
+    console.log("updated");
+  },
+  created() {
+    window.addEventListener("resize", this.myEventHandler);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.myEventHandler);
+  },
+  methods: {
+    myEventHandler(e) {
+      // your code for handling resize...
+
+      //change size
+      if (window.innerWidth < 768) {
+        this.cs = window.innerWidth * 0.9;
+      } else {
+        this.cs = 500;
+      }
+
+      //delete the existing element
+      let element = document.getElementById("paper");
+      while (element.firstChild) {
+        element.removeChild(element.firstChild);
+      }
+
+      //recreate
+      loadScript("public/js/astrochart.js")
+        .then(() => {
+          // Script is loaded, do something
+          console.log("loaded");
+          console.log(this.testString);
+          //   var data = {
+          //     planets: {
+          //       Lilith: [18],
+          //       Chiron: [18],
+          //       Pluto: [63],
+          //       Neptune: [110, 0.2],
+          //       Uranus: [318],
+          //       Saturn: [201, -0.2],
+          //       Jupiter: [192],
+          //       Mars: [210],
+          //       Moon: [268],
+          //       Sun: [281],
+          //       Mercury: [312],
+          //       Venus: [330],
+          //       NNode: [2],
+          //     },
+          //     cusps: [296, 350, 30, 56, 75, 94, 116, 170, 210, 236, 255, 274],
+          //     //切線
+          //   };
+          // window.onload = function () {
+          var radix = new astrology.Chart("paper", this.cs, this.cs).radix(
+            this.astroData
+          );
+          // Aspect calculation
+          // default is planet to planet, but it is possible add some important points:
+          radix.addPointsOfInterest({
+            As: [this.astroData.cusps[0]],
+            Ic: [this.astroData.cusps[3]],
+            Ds: [this.astroData.cusps[6]],
+            Mc: [this.astroData.cusps[9]],
+          });
+          radix.aspects();
+
+          //};
+        })
+        .catch(() => {
+          // Failed to fetch script
+          console.log("chart failed to load");
+        });
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
 #paper {
   border-radius: 50%;
   background-color: #fff;
@@ -115,6 +232,15 @@ export default {
   background: rgb(0, 16, 85);
   display: flex;
   justify-content: center;
-  width: 800px;
+}
+@media (screen) and (min-width: 768) {
+  .block {
+    width: 500px;
+    height: 500px;
+  }
+  #paper {
+    width: 500px;
+    height: 500px;
+  }
 }
 </style>
--- src/views/MyChart.vue
+++ src/views/MyChart.vue
@@ -7,7 +7,22 @@ export default {
 };
 </script>
 <template>
-  <div>
-    <Chart></Chart>
+  <div class="BG">
+    <div class="container m-5">
+      <div class="row">
+        <div class="col-md-6 col-sm-12"><Chart></Chart></div>
+
+        <div class="col-md-6 col-sm-12">ddd</div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+.BG {
+  background: rgb(0, 16, 85);
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+}
+</style>

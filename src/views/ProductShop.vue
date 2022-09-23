20e02039633caddf172cc459f597c3121b8f8544
sutsanyuan <tsanyuansu@gmail.com>
2022-09-23 10:09:37 +0800

    merged with minyi 0923

--- src/views/ProductShop.vue
+++ src/views/ProductShop.vue
@@ -19,91 +19,20 @@
             @slideChange="onSlideChange"
             @resize="onresize"
           >
-            <swiper-slide>
+            <swiper-slide v-for="(key, value) in lesson">
               <div class="shopItem">
                 <div class="image">
                   <img src="https://picsum.photos/200/200?random=1" />
                 </div>
-                <h4>塔羅牌真命天子陣</h4>
-                <p>30 mins NTD$1,000 起</p>
+                <h4>{{ key.lesson }}</h4>
+                <p>{{ key.psTime }} mins NTD$1,000 起</p>
                 <p class="swTxt">
-                  透過塔羅抽牌方式，抽出至少5張，至多全部的牌，占卜師透過牌陣，詳細分析您一年內是否有機會碰到心儀的對象！
+                  {{ key.pscontent }}
                 </p>
-                <p class="swTxt1">⚠️選購本課程可錄音、拍照，不可錄影，請自備相關設備。⚠</p>
-                <button class="orderbtn" @click="orderTo">點我預約</button>
+                <p class="swTxt1">{{ key.pstip }}</p>
+                <button class="push1" @click="orderTo(index)">點我預約</button>
               </div>
             </swiper-slide>
-            <swiper-slide>
-              <div class="shopItem">
-                <div class="image">
-                  <img src="https://picsum.photos/200/200?random=1" />
-                </div>
-                <h4>塔羅牌真命天子陣</h4>
-                <p>30 mins NTD$1,000 起</p>
-                <p class="swTxt">
-                  透過塔羅抽牌方式，抽出至少5張，至多全部的牌，占卜師透過牌陣，詳細分析您一年內是否有機會碰到心儀的對象！
-                </p>
-                <p class="swTxt1">⚠️選購本課程可錄音、拍照，不可錄影，請自備相關設備。⚠</p>
-                <button class="orderbtn" @click="orderTo">點我預約</button>
-              </div>
-            </swiper-slide>
-            <swiper-slide
-              ><div class="shopItem">
-                <div class="image">
-                  <img src="https://picsum.photos/200/200?random=1" />
-                </div>
-                <h4>塔羅牌真命天子陣</h4>
-                <p>30 mins NTD$1,000 起</p>
-                <p class="swTxt">
-                  透過塔羅抽牌方式，抽出至少5張，至多全部的牌，占卜師透過牌陣，詳細分析您一年內是否有機會碰到心儀的對象！
-                </p>
-                <p class="swTxt1">⚠️選購本課程可錄音、拍照，不可錄影，請自備相關設備。⚠</p>
-                <button class="orderbtn" @click="orderTo">點我預約</button>
-              </div></swiper-slide
-            >
-            <swiper-slide>
-              <div class="shopItem">
-                <div class="image">
-                  <img src="https://picsum.photos/200/200?random=1" />
-                </div>
-                <h4>塔羅牌真命天子陣</h4>
-                <p>30 mins NTD$1,000 起</p>
-                <p class="swTxt">
-                  透過塔羅抽牌方式，抽出至少5張，至多全部的牌，占卜師透過牌陣，詳細分析您一年內是否有機會碰到心儀的對象！
-                </p>
-                <p class="swTxt1">⚠️選購本課程可錄音、拍照，不可錄影，請自備相關設備。⚠</p>
-                <button class="orderbtn" @click="orderTo">點我預約</button>
-              </div>
-            </swiper-slide>
-            <swiper-slide
-              ><div class="shopItem">
-                <div class="image">
-                  <img src="https://picsum.photos/200/200?random=1" />
-                </div>
-                <h4>塔羅牌真命天子陣</h4>
-                <p>30 mins NTD$1,000 起</p>
-                <p class="swTxt">
-                  透過塔羅抽牌方式，抽出至少5張，至多全部的牌，占卜師透過牌陣，詳細分析您一年內是否有機會碰到心儀的對象！
-                </p>
-                <p class="swTxt1">⚠️選購本課程可錄音、拍照，不可錄影，請自備相關設備。⚠</p>
-
-                <button class="orderbtn" @click="orderTo">點我預約</button>
-              </div></swiper-slide
-            >
-            <swiper-slide
-              ><div class="shopItem">
-                <div class="image">
-                  <img src="https://picsum.photos/200/200?random=1" />
-                </div>
-                <h4>塔羅牌真命天子陣</h4>
-                <p>30 mins NTD$1,000 起</p>
-                <p class="swTxt">
-                  透過塔羅抽牌方式，抽出至少5張，至多全部的牌，占卜師透過牌陣，詳細分析您一年內是否有機會碰到心儀的對象！
-                </p>
-                <p class="swTxt1">⚠️選購本課程可錄音、拍照，不可錄影，請自備相關設備。⚠</p>
-                <button class="orderbtn" @click="orderTo">點我預約</button>
-              </div></swiper-slide
-            >
           </swiper>
         </div>
       </div>
@@ -118,8 +47,13 @@ import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/css";
 import ProductShopOrder from "../components/ProductShopOrder.vue";
 import ProductShopAbout from "../components/ProductShopAbout.vue";
+import { useStore } from "vuex";
 
 export default {
+  setup() {
+    const store = useStore();
+    return {};
+  },
   components: {
     Swiper,
     SwiperSlide,
@@ -133,23 +67,29 @@ export default {
     };
   },
   methods: {
-    orderTo() {
+    orderTo(index) {
       if (this.$data.logonType === "about") {
         this.$data.logonType = "order";
+        this.$store.commit("addStoreLesson");
+        
       } else {
         this.$data.logonType = "about";
+       
       }
     },
     onresize() {
       let clientWidth =
         document.documentElement.clientWidth || document.body.clientHeight;
-      if (clientWidth <992) {
+      if (clientWidth < 992) {
         this.slidesPerView = 1;
+      } else {
+        this.slidesPerView = 4;
       }
-      else{
-        this.slidesPerView=4;
-      }
-      
+    },
+  },
+  computed: {
+    lesson() {
+      return this.$store.state.psLesson;
     },
   },
 };
@@ -180,9 +120,7 @@ h3 {
   margin-bottom: 10px;
 }
 p {
- 
   margin-bottom: 0.5rem;
-  
 }
 .card {
   height: 300px;
@@ -204,14 +142,14 @@ p {
 }
 .swTxt {
   padding: 0 80px 0px;
-  
+
   /* text-align: left; */
 }
-.swTxt1{
- padding: 0 80px 10px;
+.swTxt1 {
+  padding: 0 80px 10px;
   /* text-align: left; */
 }
-.orderbtn {
+button {
   width: 150px;
   height: 50px;
   background: rgba(217, 217, 217, 0.25);
@@ -223,17 +161,17 @@ p {
   border-bottom: 1px solid #eee;
   margin: 0 0 40px;
 }
-.ordershop{
+.ordershop {
   margin-bottom: 20px;
 }
 
 @media screen and (min-width: 992px) {
   .swTxt {
-  padding: 0 30px 0px;
-  /* text-align: left; */
-}
-.swTxt1{
- padding: 0 30px 10px;
-}
+    padding: 0 30px 0px;
+    /* text-align: left; */
+  }
+  .swTxt1 {
+    padding: 0 30px 10px;
+  }
 }
 </style>
--- src/views/shoppingCart.vue
+++ src/views/shoppingCart.vue
@@ -1,39 +1,47 @@
 <template>
   <div class="BG">
     <div class="container-xl mt-5">
-      <h2>購物車</h2>
-      <div class="row justify-content-center myCart">
+      <div class="row">
+        <h2>購物車</h2>
         <div class="col-12 col-lg-8">
-          <div class="tittl">
-            <h3>選擇結帳商品</h3>
-            <div class="cart">
-              <h4>{{ msgt1 }}</h4>
-              <div class="item">
-                <div class="pic">
-                  <p>{{msgt2}}{{timeMsg1}}</p>
-                  <div class="cost">NT${{cost}}</div>
-                </div>
-              </div>
-              <div class="pic">
-                <p>{{msgt2}}{{timeMsg1}}</p>
-                <div class="cost">NT${{cost}}</div>
-              </div>
+          <h3>選擇結帳商品</h3>
+          <div class="itemgroup">
+            <div class="tittl">
+              <h4>{{ myCart.shopNmae }}</h4>
+            </div>
+
+            <shopping-cart-card></shopping-cart-card>
+            <div class="btnGroup">
+              <button @click="onsubmit">送出</button>
+              <button @click="remove">清除</button>
             </div>
           </div>
         </div>
-        <div class="col-12 col-lg-4">
-          <div class="tittl">
-            <h3>選擇付款方式</h3>
-            <div class="cart">
+        <div class="col-12 col-lg-4 pay1">
+          <h3>選擇付款方式</h3>
+          <div class="itemgroup">
+            <div class="tittl">
               <h4>訂單摘要</h4>
-              <div class="item">
-                <p>商品數量</p>
-                <p>{{quantity}}</p>
-              </div>
-              <div class="item">
-                <p>商品金額總計</p>
-                <p>{{totalCost}}</p>
-              </div>
+            </div>
+            <div class="count d-flex">
+              <p>商品數量</p>
+              <p>{{ myCart.psCount }}項</p>
+            </div>
+            <div class="total d-flex">
+              <p>商品總金額</p>
+              <p>NT${{ myCart.addCost }}</p>
+            </div>
+            <div class="tip">
+              <p>
+                提醒您： 台灣信用卡推薦使用EC
+                Pay結帳，使用PayPal結帳時，將酌收平台手續費。
+              </p>
+            </div>
+            <div class="image">
+              <img src="../assets/img/icons/pay1.png" alt="" />
+            </div>
+            <div class="image">
+              <img src="../assets/img/icons/pay2.png" alt="" />
             </div>
           </div>
         </div>
@@ -42,17 +50,99 @@
   </div>
 </template>
 <script>
+import shoppingCartCard from "../components/shoppingCartCard.vue";
+import { useStore } from "vuex";
 export default {
-  data() {
-    return {
-      msgt1: "思妤塔羅占卜工作坊",
-      msgt2:"塔羅牌真命天子陣",
-      timeMsg1:"30min",
-      cost:"500",
-      quantity:"2",
-      totalCost:"2500",
-      code:'',
-    };
+  setup() {
+    const store = useStore();
+    return {};
+  },
+  components: {
+    shoppingCartCard,
+  },
+  methods: {
+    remove() {
+      this.$store.commit("removeStoreInfol");
+    },
+    onsubmit() {
+      fetch("https://astria.sutsanyuan.com/Astria_api/RePWD", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json;charset =utf-8",
+        },
+        body: JSON.stringify({
+          Email: chemails,
+        }),
+      })
+        .then(function (re) {
+          return re.json();
+        })
+        .then((body) => {
+          console.log(body);
+          this.b = body.Status;
+          if (this.b == "1") {
+            this.$data.logonType = "pwd";
+            console.log("忘記密碼");
+          }
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    },
+  },
+  computed: {
+    myCart() {
+      return this.$store.state.myCart;
+    },
   },
 };
 </script>
+<style scoped>
+h2 {
+  margin: 0 0 20px 20px;
+}
+h3 {
+  margin: 0 0 15px 20px;
+}
+
+.itemgroup {
+  height: 500px;
+  border: 1px solid #eee;
+  border-radius: 10px;
+  padding: 20px;
+}
+.tittl {
+  border-bottom: 1px solid #eee;
+  margin-bottom: 20px;
+}
+.count {
+  justify-content: space-between;
+}
+.total {
+  justify-content: space-between;
+}
+
+.itemgroup .tip {
+  border-bottom: 1px solid #eee;
+  margin-bottom: 30px;
+}
+img {
+  width: 100%;
+}
+.image {
+  padding: 20px 50px;
+}
+.row {
+  margin-bottom: 60px;
+}
+button {
+  width: 120px;
+  height: 50px;
+  background: rgba(217, 217, 217, 0.25);
+  margin: 40px;
+  border-radius: 10px;
+}
+.btnGroup {
+  text-align: center;
+}
+</style>
